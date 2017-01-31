0d5fefc9166912edf7c39e77b370294c6354850d
Maxrimus <joe.p.tursi@gmail.com>
2017-01-31 17:38:45 -0500

    Basic Platform. No animations or anything just a 9x9 block platform where the blocks fall.

--- Crumble/Assets/Scripts/GameController.cs
+++ Crumble/Assets/Scripts/GameController.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public GameObject blockPref;
+    private List<GameObject> blocks = new List<GameObject>();
+    private float timer = 0;
+
 	// Use this for initialization
 	void Start () {
-		
+		for(int i = -4; i < 5; i++)
+            for(int j = -4; j < 5; j++)
+            {
+                GameObject temp = Instantiate<GameObject>(blockPref, new Vector3(i, 0, j), new Quaternion());
+                blocks.Add(temp);
+            }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer += Time.deltaTime;
+        if(timer >= 2)
+        {
+            timer -= 2;
+            drop();
+        }
 	}
+
+    void drop()
+    {
+        int num = Random.Range(0, blocks.Count - 1);
+        blocks[num].GetComponent<Block>().fallen = true;
+        blocks.RemoveAt(num);
+    }
 }
