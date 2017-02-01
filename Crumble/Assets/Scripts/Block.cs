61240fbc46b3b0bdcf32ab0fe87e6d96aaef2607
Maxrimus <joe.p.tursi@gmail.com>
2017-02-01 15:50:21 -0500

    Beginning work on Animation

--- Crumble/Assets/Scripts/Block.cs
+++ Crumble/Assets/Scripts/Block.cs
@@ -6,11 +6,12 @@ public class Block : MonoBehaviour {
 
     public bool fallen = false;
     private float fallSpeed = -0.1f;
+    Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
