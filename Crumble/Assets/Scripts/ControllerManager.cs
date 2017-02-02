a0ce12dac802f3b864304efe046f78500ef49724
Ilan Isakov <ixi7500@rit.edu>
2017-02-02 18:56:04 -0500

    Added Player and Controller Manager. Added new scene to work on player and controller stuff. Modified gravity. Changed Asset Serilization to Force Text.

---
+++ Crumble/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ControllerManager : MonoBehaviour {
+
+    private static ControllerManager instance = null;
+
+    public static ControllerManager Instance {  get { return instance; } }
+
+    private string[] joystickNames;
+    private int joystickArrayLength; //Size of Input.GetJoystickNames()
+
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Updating the controller array
+        joystickNames = Input.GetJoystickNames();
+        joystickArrayLength = joystickNames.Length;
+
+        Debug.Log(joystickArrayLength);
+	}
+}
---
+++ Crumble/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private int playerNumber; //player number in relation to the game
+    public int controllerNumber; //joystick number of player, public for debug
+
+    private Rigidbody _rigidbody;
+    private float maxSpeed;
+    private float jumpPower;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = this.GetComponent<Rigidbody>();
+
+        //controllerNumber = 1;
+        maxSpeed = 10;
+        jumpPower = 10.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //position of the movement thumbstick
+        Vector3 axisAmount = new Vector3(
+            Input.GetAxis("Horizontal_" + controllerNumber),
+            0, 
+            Input.GetAxis("Vertical_" + controllerNumber));
+
+        //moving character
+        _rigidbody.velocity = new Vector3(axisAmount.x * maxSpeed, _rigidbody.velocity.y, axisAmount.z * maxSpeed);
+
+        if (Input.GetButtonDown("Jump_" + controllerNumber))
+        {
+            _rigidbody.velocity += new Vector3(0,jumpPower, 0);
+        }
+	}
+}
